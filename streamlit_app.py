4b7bb8bfa4dcdd491d0ed5be25068240c4fd3a8c
Lucianosnx <68197433+Lucianosnx@users.noreply.github.com>
2024-07-12 17:06:21 +0000

    9

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-def calcular_preco(largura_cm, altura_cm, fator_complexidade, margem_lucro, quantidade, recorrencia, multiplicador):
+def calcular_preco(largura_cm, altura_cm, multiplicador, fator_complexidade, margem_lucro, quantidade, recorrencia):
     area = largura_cm * altura_cm
     area_referencia = 35 * 31
     preco_base = (area * 8) / area_referencia
     preco_base *= fator_complexidade
 
-    st.write(f"Preço após markup: R$ {preco_base:.2f}")
+    st.markdown(f"**Preço após markup: R$ {preco_base:.2f}**")
     preco_final = preco_base * margem_lucro
     preco_total = preco_final * quantidade * recorrencia * multiplicador
     return preco_total
@@ -15,6 +15,9 @@ st.title('Calculadora de Preços para Quadros em MDF')
 
 largura_cm = st.number_input('Largura do quadro (em centímetros):', min_value=0.0, format="%.2f")
 altura_cm = st.number_input('Altura do quadro (em centímetros):', min_value=0.0, format="%.2f")
+
+multiplicadores = {1: 1, 2: 2, 3: 3, 4: 4}
+multiplicador = st.selectbox('Multiplicador do preço final:', options=list(multiplicadores.keys()))
 
 fatores_complexidade = {1: 1.05, 2: 1.10, 3: 1.15, 4: 1.20}
 fator_complexidade = st.selectbox('Complexidade do design (1 a 4):', options=list(fatores_complexidade.keys()))
@@ -30,9 +33,7 @@ if tipo == 'Serviço':
     quantidade = st.number_input('Quantidade:', min_value=1)
     recorrencia = st.number_input('Recorrência:', min_value=1)
 
-multiplicador = st.selectbox('Multiplicador do preço final:', options=(1, 2, 3, 4))
-
 # Atualização dinâmica do preço ao modificar qualquer entrada
 if largura_cm and altura_cm:
-    resultado = calcular_preco(largura_cm, altura_cm, fatores_complexidade[fator_complexidade], margens_lucro[margem_lucro], quantidade, recorrencia, multiplicador)
-    st.write(f"Preço final: R$ {resultado:.2f}")
+    resultado = calcular_preco(largura_cm, altura_cm, multiplicadores[multiplicador], fatores_complexidade[fator_complexidade], margens_lucro[margem_lucro], quantidade, recorrencia)
+    st.markdown(f"**Preço final: R$ {resultado:.2f}**")
