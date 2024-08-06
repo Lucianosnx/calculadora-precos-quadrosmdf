af181fff318311c108d223561acda1c07d219e3a
Lucianosnx <68197433+Lucianosnx@users.noreply.github.com>
2024-08-06 21:44:54 +0000

    fix 7

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,7 +97,7 @@ uploaded_file = st.file_uploader("Upload SVG", type="svg")
 if uploaded_file is not None:
     with open("uploaded_file.svg", "wb") as f:
         f.write(uploaded_file.getbuffer())
-    complexity = calculate_svg_complexity("uploaded_file.svg", str(largura_cm*100), str(altura_cm*100))
+    complexity = calculate_svg_complexity("uploaded_file.svg", str(largura_cm*10), str(altura_cm*10))
     st.write(largura_cm*100)
     st.write(f"Taxa de Complexidade: {complexity:.2f}%")
 else:
